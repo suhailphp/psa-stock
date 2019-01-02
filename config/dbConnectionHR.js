045253497d95db93685b319a3eb359017b2def20
suhail <suhail@psa.ac.ae>
2019-01-02 12:44:46 +0400

    first prd test

--- config/dbConnectionHR.js
+++ config/dbConnectionHR.js
@@ -1,7 +1,7 @@
 
-let Config = require('./app')
+// let Config = require('./app')
 var Sequelize = require('sequelize');
-// var Config = require('./config');
+var Config = {}
 
 var dbName = Config.DB['DATABASE'] || 'PSAHR';
 var dbUsername = Config.DB['USERNAME'] || 'testhr';
