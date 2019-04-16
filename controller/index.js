c1b2cbdb3c419ca9e615c11e638062031fbb0294
suhail <suhail@psa.ac.ae>
2019-04-16 10:16:16 +0400

    first prd test

--- controller/index.js
+++ controller/index.js
@@ -1,19 +1,50 @@
 const auth = require('../middleware/auth');
 const express = require('express');
+const moment = require('moment');
 const {issueModel} = require('../models/issueModel');
 const {staffModel} = require('../models/staffModel');
+const {userModel} = require('../models/userModel');
+const {itemModel} = require('../models/itemModel');
+const {purchaseModel} = require('../models/purchaseModel');
+const {nonStockModel} = require('../models/nonStockModel');
 const {departmentModel} = require('../models/departmentModel');
 
 const router = express.Router();
 
 router.get('/',auth,async (req,res)=>{
-    issueModel.belongsTo(staffModel, {foreignKey: 'militaryNo'});
-    issueModel.belongsTo(departmentModel, {foreignKey: 'departmentID'});
-    let issues = await issueModel.findAll({include:[{model:staffModel},{model:departmentModel}],limit: 5,
-        order: [
-            ['createdOn', 'desc']
-        ] });
-    res.render('index',{issues});
+
+
+
+    let totalUser = await userModel.count();
+    let totalItem = await itemModel.count();
+
+
+    let totalIssues = await issueModel.count({
+        where: {
+            date: {
+                $gte: moment().subtract(1, 'days').toDate()
+            }
+        }
+    });
+
+
+    let Purcahse = await purchaseModel.count({
+        where: {
+            date: {
+                $gte: moment().subtract(1, 'days').toDate()
+            }
+        }
+    });
+
+    let nonStock = await nonStockModel.count({
+        where: {
+            date: {
+                $gte: moment().subtract(1, 'days').toDate()
+            }
+        }
+    });
+    let totalPurchase = Purcahse+nonStock;
+    res.render('index',{totalUser,totalItem,totalPurchase,totalIssues});
 });
 
 router.get('/menu',(req,res)=>{
