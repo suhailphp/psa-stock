39241d34ad3678feaa4523f7cf893911255e7655
suhail <suhail@psa.ac.ae>
2019-08-26 10:26:29 +0400

    recall and issue design

--- controller/login.js
+++ controller/login.js
@@ -28,9 +28,12 @@ router.post('/',async(req,res)=>{
 
     let ad = new activeDirectory(Config.DOMAIN);
 
-    res.send(ad);exit;
+
 
     ad.authenticate(userName+'@psa.local', password, function (err, auth) {
+
+        res.send(auth);exit;
+
           if (auth) {
             req.session.user = {userID:user.userID,userName:user.userName,name:user.fullName,userRole:user.userRole};
             req.session.infoMsg = {code:'success',title:'مرحبا بعودتك',content:'مرحباً بالسيد '+req.session.user.name};
