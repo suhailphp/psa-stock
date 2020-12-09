2f80de51e3d1083c27151e8410bfff8920836e17
suhailphp <suhailphp@gmail.com>
2020-12-09 08:51:17 +0400

    completion certificate model created

---
+++ models/completionCertificateModel.js
@@ -0,0 +1,63 @@
+const Joi = require("joi");
+const Sequelize = require('sequelize');
+const db = require('../config/dbConnection');
+const config = require('../config/app');
+
+
+let completionCertificateModel = db.define('completionCertificate', {
+
+    completionCertificateID:{
+        type: Sequelize.INTEGER,
+        autoIncrement : true,
+        primaryKey: true
+    },
+    referenceNo: {
+        type: Sequelize.INTEGER
+    },
+    date: {
+        type: Sequelize.DATE
+    },
+
+    amount: {
+        type: Sequelize.STRING(255)
+    },
+
+    name: {
+        type: Sequelize.STRING(255)
+    },
+
+    about: {
+        type: Sequelize.STRING(500)
+    },
+
+    notes: {
+        type: Sequelize.STRING(1000)
+    },
+
+    userID: {
+        type: Sequelize.INTEGER
+    },
+
+    createdOn: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+    updatedOn: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW
+    },
+    active: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+    }
+});
+
+
+if (config.DB['SYNC']) {
+    completionCertificateModel.sync({force: config.DB['SYNC_FORCE']});
+}
+
+
+exports.completionCertificateModel = completionCertificateModel;
+
+
